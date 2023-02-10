4b3db9da28272c98fd972af52c00a9ca0d41e09e
yanglvfeng <yanglvfeng@croot.com>
2023-02-10 14:55:23 +0800

    init 初始化项目

---
+++ build.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import path from 'path'
+import {execa} from 'execa';
+
+console.log('build start!')
+await runParallel(getTargets(), build)
+console.log('build end!')
+
+function runParallel(targets, exec){
+    const res = [];
+    targets.forEach(t => {
+        const r = exec(t);
+        res.push(r);
+    })
+    return Promise.all(res);
+}
+
+async function build(target){
+    await console.log(`mock build: ${target}`)
+    // await execa('rollup', ['-cw','--environment', `TARGET:${target}`], {stdio:"inherit"})
+}
+
+function getTargets(){
+    return fs.readdirSync(path.resolve(process.cwd(), './packages')).filter(f => fs.statSync(`packages/${f}`).isDirectory())
+}
---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import path from 'path'
+import {createRequire} from "module";
+
+import json from '@rollup/plugin-json'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
+import { babel } from '@rollup/plugin-babel';
+import commonjs from "@rollup/plugin-commonjs";
+import { uglify } from 'rollup-plugin-uglify'
+import sourceMaps from "rollup-plugin-sourcemaps";
+
+const require = createRequire(import.meta.url);
+
+process.env.TARGET = 'reactivity'
+
+const packagesDir = path.resolve(process.cwd(), 'packages');
+const packageDir = path.resolve(packagesDir, process.env.TARGET);
+const resolve = (o) => path.resolve(packageDir, o);
+const pkg = require(resolve('package.json'));
+
+const name = path.basename(packageDir);
+const OUTPUT_FORMAT = {
+    'esm-bundler': {
+        file: resolve(`dist/${name}.esm-bundler.js`),
+        format: "es",
+    },
+    'cjs': {
+        file: resolve(`dist/${name}.cjs.js`),
+        format: "cjs"
+    },
+    'global': {
+        file: resolve(`dist/${name}.global.js`),
+        format: "life"
+    }
+}
+
+const options = pkg.buildOptions;
+
+function createConfig(format,output) {
+    output.name = options.name;
+    output.sourceMap = true;
+
+    return {
+        input: resolve('src/index.js'),
+        output,
+        plugins: [
+            uglify(),
+            json(),
+            nodeResolve({
+                extensions:['.js']
+            }),
+            commonjs(),
+            babel({
+                exclude: 'node_modules/**'
+            }),
+            sourceMaps(),
+        ]
+    }
+}
+
+export default options.formats.map(format => createConfig(format, OUTPUT_FORMAT[format]))
