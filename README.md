bdaa4205a6b0f04ced2e5ef36dcc9cb47a1bde26
yanglvfeng <yanglvfeng@croot.com>
2023-02-14 15:32:40 +0800

    update 所有项目共享`node_modules`

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ monorepo-demo
 
 # TODO_LIST
 ## 整体
-- [ ] 所有项目共享`node_modules`
+- [x] 所有项目共享`node_modules`
 
 ## `demo`
 - [x] 支持`.js`书写`jsx`语法
--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -1,9 +1,10 @@
-import init from './init.js';
+import init from './init';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import {test} from '@shared/utils';
-import {print} from '@shared/base'
+import {print} from '@shared/base';
+// import {Test} from '@shared/components';
 
 print.setState(false);
 print.log("测试共享模块方法:test", test(1,2));
