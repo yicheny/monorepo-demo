ac88cfb5f61fe4b99d785b3c8f66eb6fa9dc65b2
yanglvfeng <yanglvfeng@croot.com>
2023-02-16 14:37:03 +0800

    add 指定单个应用启动

--- commands/build.js
+++ commands/build.js
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import {execa} from 'execa';
 
 console.log('build start!')
 await runParallel(getTargets(), build)
--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -2,11 +2,11 @@ import init from './init';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-// import {test} from '@shared/utils';
-// import {print} from '@shared/base';
+import {test} from '@shared/utils';
+import {print} from '@shared/base';
 import {Test} from '@shared/components';
 
-// print.setState(false);
-// print.log("测试共享模块方法:test", test(1,2));
+print.setState(false);
+print.log("测试共享模块方法:test", test(1,2));
 
 ReactDOM.render(React.createElement(Test),document.querySelector('#root'));
